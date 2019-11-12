54d1b389ed97004200db1ecce4804e7c75867f35
Scott <scott.r.ja@gmail.com>
2019-11-11 23:31:31 -0500

    First section completed

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Search Heroes</title>
+        <script src = "app.js"></script>
+    </head>
+    <body>
+        <h1>Avengers Character Search</h1>
+        <button id = "btn">Search</button>
+    </body>
